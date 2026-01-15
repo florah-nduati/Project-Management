9e1b62c7602c7d64ea8981eb60b678782a09e137
florah nyokabi nduati <florah.nduati@griffinglobaltech.com>
2026-01-15 18:04:10 +0300

    solve server issues

--- Server/Server.js
+++ Server/Server.js
@@ -18,8 +18,4 @@ app.get("/", (req, res) => {
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
