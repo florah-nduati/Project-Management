9f5a42c0daf8c7eaa7941ce217c0ada1e9a8c12d
florah nyokabi nduati <florah.nduati@griffinglobaltech.com>
2026-01-16 17:55:42 +0300

    implement creating workspaces

--- Server/Server.js
+++ Server/Server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import "dotenv/config";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import workspaceRouter from "./routes/workspaceRoutes.js";
+import { protect } from "./middlewares/authMiddleware.js";
 
 const app = express();
 
@@ -17,5 +18,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+//Routes
+app.use("/api/workspaces", protect, workspaceRouter);
+
 
 export default app; 
